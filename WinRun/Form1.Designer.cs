4ef1f54c1aa7c79516b0e3ac9ac069cd7863f441
Laurence Luo <iluo@163.com>
2020-05-07 21:25:49 +0800

    Create project WinRun

---
+++ WinRun/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace WinRun {
+    partial class Form1 {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnCcl = new System.Windows.Forms.Button();
+            this.btnBrw = new System.Windows.Forms.Button();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCcl
+            // 
+            resources.ApplyResources(this.btnCcl, "btnCcl");
+            this.btnCcl.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCcl.Name = "btnCcl";
+            this.btnCcl.UseVisualStyleBackColor = true;
+            this.btnCcl.Click += new System.EventHandler(this.btnCcl_Click);
+            // 
+            // btnBrw
+            // 
+            resources.ApplyResources(this.btnBrw, "btnBrw");
+            this.btnBrw.Name = "btnBrw";
+            this.btnBrw.UseVisualStyleBackColor = true;
+            this.btnBrw.Click += new System.EventHandler(this.btnBrw_Click);
+            // 
+            // btnOk
+            // 
+            resources.ApplyResources(this.btnOk, "btnOk");
+            this.btnOk.Name = "btnOk";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // comboBox1
+            // 
+            resources.ApplyResources(this.comboBox1, "comboBox1");
+            this.comboBox1.AllowDrop = true;
+            this.comboBox1.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawVariable;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Name = "comboBox1";
+            // 
+            // pictureBox1
+            // 
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
+            this.pictureBox1.Image = global::WinRun.Properties.Resources.run;
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnOk;
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackColor = System.Drawing.Color.White;
+            this.CancelButton = this.btnCcl;
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.btnBrw);
+            this.Controls.Add(this.btnCcl);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCcl;
+        private System.Windows.Forms.Button btnBrw;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ WinRun/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WinRun {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Shown(object sender, EventArgs e) {
+            int rectY = 190;
+            Color rectColor = Color.FromArgb(240, 240, 240);
+            Rectangle rect = new Rectangle(0, rectY, Width, Height - rectY);
+            Brush rectBrush = new SolidBrush(rectColor);
+            Graphics g = this.CreateGraphics();
+            g.Clear(BackColor);
+            g.FillRectangle(rectBrush, rect);
+
+            comboBox1.SelectedIndex = -1;
+            comboBox1.Text = "";
+        }
+
+        private void btnOk_Click(object sender, EventArgs e) {
+            string exe = comboBox1.Text;
+            int idxEpt = exe.IndexOf(' ');
+            try {
+                if (idxEpt == -1) {
+                    Process.Start(exe);
+                } else {
+                    string exeA = exe.Substring(0, idxEpt);
+                    string exeB = exe.Substring(idxEpt + 1);
+                    Process.Start(exeA, exeB);
+                }
+                Application.Exit();
+            } catch {
+                MessageBox.Show("Windows cannot find '" + comboBox1.Text + "'. Make sure you typed the name correctly, and then try again.", 
+                    comboBox1.Text, MessageBoxButtons.OK,
+                    MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+            }
+        }
+
+        private void btnCcl_Click(object sender, EventArgs e) {
+            Application.Exit();
+        }
+
+        private void btnBrw_Click(object sender, EventArgs e) {
+            OpenFileDialog dialog = new OpenFileDialog {
+                Multiselect = false,
+                Title = "Browse",
+                Filter = "Programs (*.exe;*.pif;*.com;*.bat;*.cmd)|*.exe;*.pif;*.com;*.bat;*.cmd"
+            };
+            string file = null;
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK) {
+                file = dialog.FileName;
+            }
+            comboBox1.Text = file;
+        }
+    }
+}

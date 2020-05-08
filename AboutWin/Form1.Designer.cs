6fda5d76f163ebd71089b5653f036b0c505ece88
Laurence Luo <iluo@163.com>
2020-05-08 12:35:07 +0800

    First version of AboutWin

---
+++ AboutWin/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace AboutWin {
+    partial class Form1 {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblVersion = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblCopyright = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.lblUsr = new System.Windows.Forms.Label();
+            this.lblOrg = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::AboutWin.Properties.Resources.win10;
+            this.pictureBox1.Location = new System.Drawing.Point(114, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(481, 103);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(76, 144);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(142, 19);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Microsoft Windows";
+            // 
+            // lblVersion
+            // 
+            this.lblVersion.AutoSize = true;
+            this.lblVersion.Font = new System.Drawing.Font("Tahoma", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblVersion.Location = new System.Drawing.Point(77, 168);
+            this.lblVersion.Name = "lblVersion";
+            this.lblVersion.Size = new System.Drawing.Size(263, 19);
+            this.lblVersion.TabIndex = 2;
+            this.lblVersion.Text = "Version 1909 (OS Build 18363.778)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(77, 192);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(371, 19);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "© 2019 Microsoft Corporation. All rights Reserved.";
+            // 
+            // lblCopyright
+            // 
+            this.lblCopyright.Font = new System.Drawing.Font("Tahoma", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCopyright.Location = new System.Drawing.Point(77, 239);
+            this.lblCopyright.Name = "lblCopyright";
+            this.lblCopyright.Size = new System.Drawing.Size(538, 57);
+            this.lblCopyright.TabIndex = 4;
+            this.lblCopyright.Text = "The Windows 10 Pro operating system and its user interface are protected \r\nby tra" +
+    "demark and other pending or existing intellectual property rights in \r\nthe Unite" +
+    "d States and other countries/regions.\r\n";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.Font = new System.Drawing.Font("Tahoma", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel1.LinkArea = new System.Windows.Forms.LinkArea(35, 32);
+            this.linkLabel1.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(102)))), ((int)(((byte)(204)))));
+            this.linkLabel1.Location = new System.Drawing.Point(76, 386);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(451, 42);
+            this.linkLabel1.TabIndex = 5;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "This product is licensed under the Microsoft Software License Terms to:";
+            this.linkLabel1.UseCompatibleTextRendering = true;
+            // 
+            // lblUsr
+            // 
+            this.lblUsr.AutoSize = true;
+            this.lblUsr.Font = new System.Drawing.Font("Tahoma", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsr.Location = new System.Drawing.Point(98, 433);
+            this.lblUsr.Name = "lblUsr";
+            this.lblUsr.Size = new System.Drawing.Size(75, 19);
+            this.lblUsr.TabIndex = 6;
+            this.lblUsr.Text = "usr name";
+            // 
+            // lblOrg
+            // 
+            this.lblOrg.AutoSize = true;
+            this.lblOrg.Font = new System.Drawing.Font("Tahoma", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOrg.Location = new System.Drawing.Point(98, 457);
+            this.lblOrg.Name = "lblOrg";
+            this.lblOrg.Size = new System.Drawing.Size(77, 19);
+            this.lblOrg.TabIndex = 7;
+            this.lblOrg.Text = "org name";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(144F, 144F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(688, 574);
+            this.Controls.Add(this.lblOrg);
+            this.Controls.Add(this.lblUsr);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.lblCopyright);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblVersion);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "About Windows";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblVersion;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblCopyright;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label lblUsr;
+        private System.Windows.Forms.Label lblOrg;
+    }
+}
+
---
+++ AboutWin/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AboutWin {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            Version winver = Environment.OSVersion.Version;
+            int majorVer = winver.Major;
+            int minorver = winver.Minor;
+            int revisver = winver.Revision;
+            string winstr = Environment.OSVersion.ServicePack;
+
+            ManagementClass mc = new ManagementClass("Win32_OperatingSystem");
+            ManagementObjectCollection moc = mc.GetInstances();
+            ManagementObject mo = moc.OfType<ManagementObject>().FirstOrDefault();
+            string wincap = mo["Caption"].ToString();
+
+            string relsever = null;
+            string buildver = null;
+            string upbldver = null;
+            string usrName = null;
+            string orgName = null;
+            using (var hklmKey = Microsoft.Win32.Registry.LocalMachine)
+            using (var subKey = hklmKey.OpenSubKey(@"SOFTWARE\Microsoft\Windows NT\CurrentVersion")) {
+                if (subKey != null) {
+                    relsever = subKey.GetValue("ReleaseId") as string;
+                    buildver = subKey.GetValue("CurrentBuildNumber") as string;
+                    upbldver = subKey.GetValue("UBR").ToString() as string;
+                    usrName = subKey.GetValue("RegisteredOwner") as string;
+                    orgName = subKey.GetValue("RegisteredOrganization") as string;
+                }
+            }
+
+            lblVersion.Text = "Version " + relsever + " (OS Build " + buildver + "." + upbldver + ")";
+
+            lblCopyright.Text = "The " + wincap.Replace("Microsoft ", "") + 
+                " operating system and its user interface are protected by trademark and other pending or existing intellectual property rights in the United States and other countries/regions.";
+
+            lblUsr.Text = usrName;
+            lblOrg.Text = orgName == null ? "org name" : orgName ;
+        }
+
+        private void Form1_Shown(object sender, EventArgs e) {
+            int lineY = 125;
+            Color lineColor = Color.FromArgb(160, 160, 160);
+            Pen linePen = new Pen(lineColor);
+            Graphics g = this.CreateGraphics();
+            g.Clear(BackColor);
+            g.DrawLine(linePen, new Point(24, lineY), new Point(Width - 48, lineY));
+            g.DrawLine(new Pen(Color.White), new Point(24, lineY + 1), new Point(Width - 48, lineY + 1));
+        }
+    }
+}
